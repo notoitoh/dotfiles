1677838376b9ed8f576c739adc234a987f95005d
nohto356 <notoito.public+github@gmail.com>
2022-12-28 01:48:39 +0900

    brewインストール時のディレクトリの指定を修正

--- src/install/brew/install.sh
+++ src/install/brew/install.sh
@@ -3,7 +3,7 @@ echo "Install brew..."
 if [[ $(check-os) == "linux" ]]; then
     if [[ ! $(which brew) ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/nohtoit/.profile
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.profile"
         eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
         sudo apt-get install build-essential
         brew install gcc
