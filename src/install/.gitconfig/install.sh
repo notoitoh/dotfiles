52fc9f5fa3e7839cd156fdd5d85996cba590ed96
nohto356 <notoito.public+github@gmail.com>
2023-01-06 21:46:04 +0900

    helper削除コマンドの実装

--- src/install/.gitconfig/install.sh
+++ src/install/.gitconfig/install.sh
@@ -1,6 +1,7 @@
 echo "Install .gitconfig..."
 
 symlink-move "${SCRIPT_DIR}/resources/.gitconfig" "${HOME}/.gitconfig"
-git config --global credential.helper ''
+git config --global credential.helper '${EMPTY}'
+cp ${SCRIPT_DIR}/resources/.gitconfig ${SCRIPT_DIR}/resources/.gitconfig.tmp && envsubst < ${SCRIPT_DIR}/resources/.gitconfig.tmp > ${SCRIPT_DIR}/resources/.gitconfig && rm ${SCRIPT_DIR}/resources/.gitconfig.tmp
 
 echo "Install .gitconfig completed."
