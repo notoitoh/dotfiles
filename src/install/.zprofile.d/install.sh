f58598606ea6864f9665a0333f6349764269b00b
nohto356 <notoito.public+github@gmail.com>
2023-01-12 01:13:40 +0900

    brewパスの見直し

--- src/install/.zprofile.d/install.sh
+++ src/install/.zprofile.d/install.sh
@@ -2,13 +2,13 @@ echo "Install zprofile..."
 
 if [[ $(check-os) == "linux" ]]; then
   cat << EOF > ${SCRIPT_DIR}/resources/.zprofile.d/generated.local.sh
-eval "\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval "\$(${BREW_PATH} shellenv)"
 EOF
 fi
 if [[ $(check-os) == "mac" ]]; then
   cat << EOF > ${SCRIPT_DIR}/resources/.zprofile.d/generated.local.sh
 export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-eval "\$(/opt/homebrew/bin/brew shellenv)"
+eval "\$(${BREW_PATH} shellenv)"
 EOF
 fi
 
