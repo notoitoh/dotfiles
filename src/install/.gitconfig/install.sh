b4b849b3f97bb93195271717fbe158c3e6990b26
nohto356 <notoito.public+github@gmail.com>
2023-02-07 08:56:59 +0900

    git config修正

--- src/install/.gitconfig/install.sh
+++ src/install/.gitconfig/install.sh
@@ -2,6 +2,7 @@ echo "Install .gitconfig..."
 
 symlink-move "${SCRIPT_DIR}/resources/.gitconfig" "${HOME}/.gitconfig"
 git config --global credential.helper '${EMPTY}'
+git config --global pull.rebase false
 cp ${SCRIPT_DIR}/resources/.gitconfig ${SCRIPT_DIR}/resources/.gitconfig.tmp && envsubst < ${SCRIPT_DIR}/resources/.gitconfig.tmp > ${SCRIPT_DIR}/resources/.gitconfig && rm ${SCRIPT_DIR}/resources/.gitconfig.tmp
 
 echo "Install .gitconfig completed."
